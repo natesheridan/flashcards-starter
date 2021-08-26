20c8fe27ba87ef767af1604566fbfae50c9f01d6
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-08-26 14:29:45 -0600

    Initial commit

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,27 @@
+const Deck = require('../src/Deck.js');
+const Card = require('../src/Card.js');
+const Round = require('../src/Round.js');
+const Turn = require('../src/Turn.js');
+const util = require('./util');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+let currentDeck = prototypeQuestions;
+
+
+
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.rawDeckArray = currentDeck;
+    this.instanciatedCardsArray = undefined;
+    this.deck;
+  }
+  start(){
+    this.instanciatedDeckArray = this.rawDeckArray.map(function(rawQuestionObj){
+      new Card(rawQuestionObj.id, rawQuestionObj.question, rawQuestionObj.answers, rawQuestionObj.correctAnswer)
+    })
+
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.

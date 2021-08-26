e6f596453db216388a430e897617b5e9e87cf94f
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-08-26 14:29:59 -0600

    Initial commit

---
+++ test/Game-test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const Game = require('../src/Game.js');
+
+describe('Game', function(){
+    beforeEach(function(){
+        var testGame1 = new Game();
+    })
+    it('Should be able to create a deck based off of currentDeck arr', function(){
+
+    }
+})

e8f93086cb53d101943981022211013b502777a4
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-08-25 22:39:05 -0600

    Functional round class

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,47 @@
 class Round{
-    constructor(options){
-        
+    constructor(deck){
+        this.deck = deck.deck;
+        this.incorrectGuesses = [];
+        this.correctGuesses = [];
+        this.turns = 0;
+        this.currentCard = this.deck[this.turns]
+        this.isCompleted = false;
     }
+    returnCurrentCard(){
+        return this.currentCard;
+    }
+    takeTurn(guess){
+        this.currentCard = this.deck[this.turns]
+        let check = (guess===this.currentCard.correctAnswer)
+        if(this.turns===this.deck.length){
+            this.isCompleted = true
+        }
+        switch(check){
+            case true:
+                this.correctGuesses.push(this.currentCard)
+                this.endRound()
+                return `Correct!`;
+            case false:
+                this.incorrectGuesses.push(this.currentCard)
+                this.endRound()
+                return `Incorrect!`;
+                
+        }
+    }
+    calculatePercentCorrect(){
+        let percentage = `${(this.correctGuesses.length/this.deck.length*100)}%`
+        return `Percentage Correct : ${percentage}`
+    }
+    endRound(){
+        switch(this.isCompleted){
+            case (true):
+                console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()} of the questions correctly!`);
+            default: 
+                this.turns++;
+                this.currentCard = this.deck[this.turns]
+        }
+    }
+
 }
+
+module.exports = Round;
